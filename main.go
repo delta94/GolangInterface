a2cdae6cdcc276fab6bb49cbc6800e5f3b96607d
hlamnguyen <lam421996@gmail.com>
2021-01-09 12:47:58 +0700

    Update code functional

--- main.go
+++ main.go
@@ -1,21 +1,71 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-)
+import "log"
 
-type Message struct {
-	Name string
-	Body string
+type House struct {
+	Material     string
+	HasFireplace bool
+	Floors       int
+}
+
+// NewHouse now takes a slice of option as the rest arguments
+func NewHouse(opts ...HouseOption) *House {
+	const (
+		defaultFloors       = 2
+		defaultHasFireplace = true
+		defaultMaterial     = "wood"
+	)
+
+	h := &House{
+		Material:     defaultMaterial,
+		HasFireplace: defaultHasFireplace,
+		Floors:       defaultFloors,
+	}
+
+	// Loop through each option
+	for _, opt := range opts {
+		// Call the option giving the instantiated
+		// *House as the argument
+		opt(h)
+	}
+
+	// return the modified house instance
+	return h
+}
+
+type HouseOption func(*House)
+
+func WithConcrete() HouseOption {
+	return func(h *House) {
+		h.Material = "concrete"
+	}
+}
+
+func WithoutFireplace() HouseOption {
+	return func(h *House) {
+		h.HasFireplace = false
+	}
+}
+
+func WithFloors(floors int) HouseOption {
+	return func(h *House) {
+		h.Floors = floors
+	}
+}
+
+func adder() func(int) int {
+	sum := 1
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
 
 func main() {
-	b := []byte(`{"Name":"Bob","Body":"Pickle"}`)
-	var m Message
-	err := json.Unmarshal(b, &m)
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println(m)
+	h := NewHouse(
+		WithConcrete(),
+		WithoutFireplace(),
+		WithFloors(3),
+	)
+	log.Println(*h)
 }
